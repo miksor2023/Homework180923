e93e449d79913a395d064be2616ba8a29055ac1f
miksor2023 <miksor@yandex.ru>
2023-09-19 20:56:01 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //
+        //задача 1
+        System.out.println("\nЗадача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        //
+        //задача 2
+        System.out.println("\nЗадача 2");
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUpper);
+        //
+        //задача 3
+        System.out.println("\nЗадача 3");
+
     }
 }
